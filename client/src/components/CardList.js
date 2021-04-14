b375872759f1a37741ad1ddb9c354ad43cac1d8d
tedtke <tedtke514@gmail.com>
2021-04-13 21:04:49 -0500

    added styling to cards list

--- client/src/components/CardList.js
+++ client/src/components/CardList.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Image } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 
 
 const CardList = ({ cards }) => {
     return (
         <div>
-            {cards.map((card) => 
-            <ul>
-                <li key={card.id}>{card.name} - {card.quantity} - {card.card_type}</li>
-                <Image id="card-image" src={card.image} alt={card.name} size='small'/>
-            </ul>)}
+          <Card  raised image='src' fluid={true}>
+            {cards.map((card) =>
+              <Card.Content textAlign='center'>
+                <>
+                  <Image id="card-image"  src={card.image} alt={card.name} wrapped-ui={false} size='medium'/>
+                   <Card.Header key={card.id}>{card.name}</Card.Header>
+
+                    <Card.Meta>Quantity: {card.quantity}</Card.Meta>
+                    <Card.Description>Card Type: {card.card_type}</Card.Description>
+                 </>
+              </Card.Content>
+            )}
+            </Card>
         </div>
+
     )
 }
 
@@ -20,3 +29,28 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(CardList);
+//
+// import { Card, Icon, Image } from 'semantic-ui-react'
+
+// const CardExampleCard = () => (
+//   <Card>
+//     <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+//     <Card.Content>
+//       <Card.Header>Matthew</Card.Header>
+//       <Card.Meta>
+//         <span className='date'>Joined in 2015</span>
+//       </Card.Meta>
+//       <Card.Description>
+//         Matthew is a musician living in Nashville.
+//       </Card.Description>
+//     </Card.Content>
+//     <Card.Content extra>
+//       <a>
+//         <Icon name='user' />
+//         22 Friends
+//       </a>
+//     </Card.Content>
+//   </Card>
+// )
+//
+// export default CardExampleCard
