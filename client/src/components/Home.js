ab8d77631067b9eee48e80244ecccf42a9c39320
tedtke <tedtke514@gmail.com>
2021-04-16 08:13:04 -0500

    ui improvements to the home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Image} from 'semantic-ui-react';
 
 const Home = () => {
     return (
@@ -8,6 +8,7 @@ const Home = () => {
             <br/>
             <Header as='h1'>Home</Header>
            <p>Welcome to your virtual deckbox!</p>
+           <Image src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fantasywelt.de%2Fmedia%2Fimage%2Fproduct%2F56909%2Flg%2Fdeckbox-orange.jpg&f=1&nofb=1" />
           </Container>
         </div>
     )
